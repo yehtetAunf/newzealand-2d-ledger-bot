3046ad5572e7f14cb36e0a6181f1cc0fd213ad98
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-02 17:58:26 +0630

    Add licensed_groups table to schema

--- schema.sql
+++ schema.sql
@@ -11,3 +11,16 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TEXT,
     updated_at TEXT
 );
+CREATE TABLE IF NOT EXISTS licensed_groups (
+    group_id INTEGER PRIMARY KEY,
+    owner_id INTEGER,
+    group_title TEXT,
+
+    status TEXT DEFAULT 'pending',
+    plan TEXT DEFAULT 'none',
+
+    expires_at TEXT,
+
+    created_at TEXT,
+    updated_at TEXT
+);
